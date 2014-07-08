e042c2c0cdcc09ee1b107fbed04b29d509f7e924
mazurkevich <anyamazur@gmail.com>
2014-07-08 16:56:18 +0400

    UserView
    UI for the sending twitt feature.
    Navigation between UserView and Login View

---
+++ Twitter_Killer/UserView.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace Twitter_Killer
+{
+    public partial class UserView
+    {
+        public UserView()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            LoginView loginView = new LoginView();
+            loginView.Show();
+        }
+    }
+}
